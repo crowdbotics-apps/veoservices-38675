f65df766429f641cde1b49038cbcd446d65b0be3
Crowdbotics <team@crowdbotics.com>
2023-02-03 02:29:23 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "veoservices-38675",
+  url: "https://veoservices-38675.botics.co",
+  api: "https://veoservices-38675.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
